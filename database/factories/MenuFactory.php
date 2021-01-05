c29397940feb65b4128df2e79b6d7b8a83e6eb9d
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2021-01-05 07:47:12 +0630

    Working on the menu module

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Menu;
+use Faker\Generator as Faker;
+
+$factory->define(Menu::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->paragraph(2, true),
+        'price' => $faker->numberBetween(99,999)
+    ];
+});
